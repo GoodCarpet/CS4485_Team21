74ce2efbb47f23d8f851f8a72e4c3db0e0bbabc1
Aniq Sunesara <102699076+AniqSunesara@users.noreply.github.com>
2026-09-18 22:03:47 -0500

    Updated version with DB connection

--- sentence-builder/src/main/java/com/oetd/sentencebuilder/HelloApplication.java
+++ sentence-builder/src/main/java/com/oetd/sentencebuilder/HelloApplication.java
@@ -6,10 +6,25 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.*;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+        try {
+            DataBaseConnection db = new DataBaseConnection();
+            //db.updateStatement("Insert into Token (token_str) values ('world');");
+            ResultSet rs = db.askQuery("select token_str from Token;");
+            while(rs.next()){
+                System.out.println(rs.getString(1));
+            }
+
+        } catch (SQLException e){
+            System.out.println("db error: " + e.getMessage());
+        }
+
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
--- sentence-builder/src/main/java/module-info.java
+++ sentence-builder/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.oetd.sentencebuilder {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.oetd.sentencebuilder to javafx.fxml;
